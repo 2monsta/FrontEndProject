d7bbb989a7b6d97ae7353de618baea475b4c1fff
Jong Park <maclay13531@gmail.com>
2017-10-17 16:14:14 -0400

    20171017 basic layout v6

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,4 @@ $(document).ready(()=>{
 	$("#log-in").click(()=>{
 		$('#myModal').modal('show');
 	})
-	var b = $(".start-location").val();
-	var a = `<a href="map.html?zip=${b}" target="blank" class="btn btn-primary btn-block">GO!</a>`;
-	$(".search-link").html(a);
 })
