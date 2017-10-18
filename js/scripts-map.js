8fd40807209734f93b05b0a92501ac9fa0988839
Jason Li <Yunf.li37@gmail.com>
2017-10-18 11:22:00 -0400

    load google maps

--- js/scripts-map.js
+++ js/scripts-map.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
 	var google = window.google;
-	
-	
+	var myLatLng = {
+		lat: 40.0000,
+		lng: -98.0000
+	}
+	var map = new google.maps.Map(document.getElementById("map"), {
+		zoom: 4,
+		center: myLatLng
+	});
 });
